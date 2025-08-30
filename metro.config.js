8b435dddc387ab3be8c3c1dee41062d42332dac5
Sigurdmazanti <sigmn@live.dk>
2025-08-30 10:04:13 +0200

    Configure metro/expo doctor

--- metro.config.js
+++ metro.config.js
@@ -5,15 +5,11 @@
 const { getDefaultConfig } = require('expo/metro-config')
 const { withTamagui } = require('@tamagui/metro-plugin')
 
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-})
+const config = getDefaultConfig(__dirname)
 
 config.resolver.sourceExts.push('mjs')
 
 module.exports = withTamagui(config, {
   components: ['tamagui'],
   config: './tamagui.config.ts',
-  outputCSS: './tamagui-web.css',
 })
